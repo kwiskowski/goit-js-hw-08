0c5cfec886b43389c7621cf8e0bdb3e2a79beba0
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-08-22 23:30:34 +0200

    simplelightbox added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('ul.gallery');
+const galleryImages = galleryItems
+  .map(
+    image =>
+      `<li><a class="gallery__item" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" title="${image.description}" alt="${image.description}" />
+    </a></li>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryImages);
 
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'img') return;
+  const handleEscapeKey = e => {
+    if (e.key === 'Escape') instance.close();
+  };
+  const instance = simpleLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onShow: () => {
+        document.addEventListener('keydown', handleEscapeKey);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', handleEscapeKey);
+      },
+    }
+  );
+  instance.show();
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  overlayOpacity: 0.5,
+});
 console.log(galleryItems);
