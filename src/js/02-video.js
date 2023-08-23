f57131278493008683444f2f7af8a34f321a8965
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-08-23 22:24:39 +0200

    vimeo script

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', function (data) {
+  // data is an object containing properties specific to that event
+});
+
+const data = {
+  duration: 61.857,
+  percent: 0.049,
+  seconds: 3.034,
+};
