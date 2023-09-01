96301f5a749dabb5452a40fecf752e3c7f2e5aba
kwiskowski <krzysztof.wiskowski@gmail.com>
2023-09-01 13:14:21 +0200

    task 3 final (i hope) update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,48 +5,35 @@ const trottled = _.throttle(InputEvent, 500);
 // (͡° ͜ʖ ͡°)
 
 const input = document.querySelector('input');
-const textArea = document.querySelector('textarea');
 
 input.addEventListener('input', e => {
   const emailOutput = (input.textContent = e.currentTarget.value);
-  localStorage.setItem('email', emailOutput);
-  console.log(emailOutput);
-});
-
-textArea.addEventListener('input', e => {
   const messageOutput = (input.textContent = e.currentTarget.value);
-  localStorage.setItem('message', messageOutput);
-  console.log(messageOutput);
+
+  const formOutput = {
+    email: emailOutput,
+    message: messageOutput,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formOutput));
 });
 
-const emailOutput = localStorage.getItem('email');
-const messageOutput = localStorage.getItem('message');
-
-const formOutput = {
-  email: emailOutput,
-  message: messageOutput,
-};
-
-console.log(formOutput);
-
-localStorage.setItem('feedback-form-state', JSON.stringify(formOutput));
 const savedForm = localStorage.getItem('feedback-form-state');
 const parsedForm = JSON.parse(savedForm);
 
-// console.log(typeof savedForm);
-// console.log(typeof parsedForm);
-
 window.onload = function () {
-  document.getElementById('email_input').value = parsedForm.email;
-  document.getElementById('message_input').value = parsedForm.message;
+  if (localStorage.getItem('feedback-form-state') !== null) {
+    document.getElementById('email_input').value = parsedForm.email;
+    document.getElementById('message_input').value = parsedForm.message;
+  }
+  return;
 };
-localStorage.removeItem('email');
-localStorage.removeItem('message');
 
 function handleSubmit(event) {
   event.preventDefault();
-  document.getElementById('email_input').value = parsedForm.email;
-  document.getElementById('message_input').value = parsedForm.message;
+  document.getElementById('email_input').value = '';
+  document.getElementById('message_input').value = '';
+  localStorage.getItem('feedback-form-state');
   console.log(parsedForm);
   localStorage.removeItem('feedback-form-state');
 }
@@ -54,6 +41,9 @@ function handleSubmit(event) {
 const registerForm = document.querySelector('.feedback-form');
 registerForm.addEventListener('submit', handleSubmit);
 
-console.log(localStorage.getItem('feedback-form-state'));
+console.log(
+  'feedback-form-state:',
+  localStorage.getItem('feedback-form-state')
+);
 
 // (,,◕　⋏　◕,,)
